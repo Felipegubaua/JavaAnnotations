8553d48d96ddba32decca9d9b5020bf3c24cc024
FelipeGubaua <felipegubaua12@gmail.com>
2024-08-19 19:16:01 -0300

    Adicionando no projeto a interface

--- ProjectsEclipse/StudyingUdemy/src/model/service/BrazilTaxService.java
+++ ProjectsEclipse/StudyingUdemy/src/model/service/BrazilTaxService.java
@@ -1,6 +1,6 @@
 package model.service;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService{
 
 	public Double tax(double amount) {
 		return (amount <= 100 )? amount*0.2 : amount*0.15;
